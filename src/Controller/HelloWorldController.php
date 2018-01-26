91d9669c4659c1c4f02d6cf51cbf5ed3dc89539f
Bert-Jan de Lange <bert-jan@nostradamus.nu>
2018-01-26 10:05:58 +0100

    Extenden van de basis controller maakt de container-as-service automatisch public. Dan moet hij dus expliciet public gemaakt worden.

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -7,14 +7,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 
-class HelloWorldController extends Controller
+class HelloWorldController
 {
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/hello", name="hello", methods="GET")
      */
-    public function hello(Environment $twig): Response
+    public function hello(): Response
     {
-        return new Response($twig->render('hello.html.twig', [
+        return new Response($this->twig->render('hello.html.twig', [
             'message' => 'Hello world.. enzo..',
         ]));
     }
