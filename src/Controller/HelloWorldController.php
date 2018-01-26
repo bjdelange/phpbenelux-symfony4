767f91f18a117b7360b5d57e6bb3cca53e01f716
Bert-Jan de Lange <bert-jan@nostradamus.nu>
2018-01-26 09:41:08 +0100

    hello route naar annotation

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class HelloWorldController
 {
+    /**
+     * @Route("/hell2o", name="hello", methods="GET")
+     */
     public function hello(): Response
     {
-        return new Response('Werkt het al ?');
+        return new Response('Hello world.. enzo..');
     }
 }
