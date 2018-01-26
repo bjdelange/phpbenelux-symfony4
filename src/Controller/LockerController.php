498ec76bee03c9794b6e09c5ed5d32f23a6a1d0b
Bert-Jan de Lange <bert-jan@nostradamus.nu>
2018-01-26 12:17:15 +0100

    named route in template

--- src/Controller/LockerController.php
+++ src/Controller/LockerController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\LockerRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class LockerController extends Controller
@@ -17,5 +18,13 @@ class LockerController extends Controller
         return $this->render('locker/list.html.twig', [
             'lockers' => $repository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/deposit", name="locker_deposit")
+     */
+    public function pickUpPackage(Request $request/*, LockerManager $lockerManager*/): Response
+    {
+        // jada jada
     }
 }
